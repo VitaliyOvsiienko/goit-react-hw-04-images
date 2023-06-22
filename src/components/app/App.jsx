1d9ae27058e24e8937900c694dc0d1afbcfc19fa
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-06-22 10:50:12 +0300

    Update App, imageGallery

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,20 +1,68 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './App.styled';
 import { Searchbar } from "../searchbar";
 import { ImageGallery } from "../imageGallery";
+import { Button } from 'components/button';
+import { Loader } from 'components/loader';
+import { getGalleryImages } from '../../serviceApi';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 export const App = () => {
   const [query, setQuery] = useState('');
+  const [gallery, setGallery] = useState([]);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const handleSearchFormSubmit = (query) => {
-    setQuery(query);
+  useEffect(() => {
+    if (query) fetchImages();
+
+    async function fetchImages() {
+      setIsLoading(true);
+      try {
+        const { hits, total, totalHits } = await getGalleryImages(query, page);
+
+        if (total === 0) {
+          const error = new Error('Houston, we have a problem')
+          setError(error);
+          return;
+        };
+
+        setGallery(prev => [...prev, ...hits]);
+        setTotalPages(Math.floor(totalHits / 12));
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      };
+    };
+  }, [page, query]);
+
+  const searchFormSubmitHandler = (searchQuery) => {
+    if (searchQuery === query) {
+      Notify.failure('This word has already been searched for. Try another one')
+      return;
+    }
+    setQuery(searchQuery);
+    setPage(1);
+    setGallery([]);
+    setError(null);
   };
 
+  const handleLoadMore = () => setPage(prevPage => prevPage + 1);
+
   return (
-    <Container>
-      <Searchbar onSubmit={handleSearchFormSubmit} />
-      <ImageGallery query={query} />
+    <>
+      <Container>
+      <Searchbar onSubmit={searchFormSubmitHandler} />
+      {isLoading && <Loader />};
+      {error && Notify.failure(`${error.message}`)};
+      {gallery.length > 0 && <ImageGallery gallery={gallery} />}
+      {gallery.length > 0 && page < totalPages && <Button onClick={handleLoadMore}>Load more</Button>}
     </Container>
+    </>
   );
+
 };
--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,106 +1,25 @@
-import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Gallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/imageGalleryItem';
-import { Button } from 'components/button';
-import { Loader } from 'components/loader';
-import { getGalleryImages } from '../../serviceApi';
+import { Gallery } from './ImageGallery.styled';
 
-export const ImageGallery = ({ query }) => {
-  const [gallery, setGallery] = useState([]);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
-  const [page, setPage] = useState(1);
-  const [isShowButton, setIsShowButton] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchGalleryImages = async (inquiry, pageNum) => {
-        try {
-          const { hits, total } = await getGalleryImages(inquiry, pageNum);
-
-          if (total === 0) {
-            const error = new Error('Houston, we have a problem');
-            setError(error);
-            setStatus('rejected');
-            return;
-          }
-
-          setGallery(hits);
-          setStatus('resolved');
-          setIsShowButton(hits.length >= 12);
-        } catch (error) {
-          setError(error);
-          setStatus('rejected');
-        }
-      };
-
-      if (query) {
-        setStatus('pending');
-        setPage(1);
-        setGallery([]);
-
-        await fetchGalleryImages(query, 1);
-      }
-    };
-
-    fetchData();
-  }, [query]);
-
-  useEffect(() => {
-    const fetchMoreData = async () => {
-      try {
-        const { hits } = await getGalleryImages(query, page);
-        setGallery(prevGallery => [...prevGallery, ...hits]);
-        setIsShowButton(hits.length >= 12);
-      } catch (error) {
-        setError(error);
-        setStatus('rejected');
-      }
-    };
-
-    if (page > 1) {
-      fetchMoreData();
-    }
-  }, [query, page]);
-
-  const handleLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  if (status === 'pending') {
-    return <Loader />;
-  }
-
-  if (status === 'rejected') {
-    Notify.failure(`${error.message}`);
-    return null;
-  }
-
-  if (status === 'resolved') {
-    return (
-      <>
-        <Gallery>
-          {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
-            <ImageGalleryItem
-              key={id}
-              url={webformatURL}
-              alt={tags}
-              largeImage={largeImageURL}
-            />
-          ))}
-        </Gallery>
-        {isShowButton && (
-          <Button onClick={handleLoadMore}>Load more</Button>
-        )}
-      </>
-    );
-  };
-
-  return null;
+export const ImageGallery = ({ gallery }) => {
+  return (
+    <Gallery>
+      {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          url={webformatURL}
+          alt={tags}
+          largeImage={largeImageURL}
+        />
+      ))}
+    </Gallery>
+  );
 };
 
 ImageGallery.propTypes = {
-  query: PropTypes.string.isRequired,
+  gallery: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired
+  })),
 };
